be49b71e3ee7cd3b625d8c570c31900437b50510
張書懷 <junsuwhy@yahoo.com.tw>
2013-08-26 23:29:36 +0800

    更正不能用的問題, 新增取得下一季的date的函式
    1.修正了nextMonthFirstDay()的錯誤
    2.新增nextMonthFirstDay()
    3.新增自己用的ad()

--- whyWidget.js
+++ whyWidget.js
@@ -8,14 +8,42 @@ arrFromObj=function(myObj){
 	return arr;
 }
 
+//99-->"1999", 100-->"2000", 113-->"2013"
+ad=function(numYear){
+	if(Math.floor(numYear/100)==1){
+		return "20"+numYear.toString().substr(1);
+	}else{
+		return "19"+numYear.toString();
+	}
+
+}
+
 //傳回下個月一號的date物件
 nextMonthFirstDay=function(strDate){
 	date=new Date(strDate);
-	year=date.getYear;
-	month=date.getMonth;
+	year=date.getYear();
+	month=date.getMonth();
 	if(month==11){
 		year+=1;
 		month=1;
 	}
-	return new Date(year.subStr(1)+"/"+month+"/1");
+	else{
+		month+=2;
+	}
+	return new Date(ad(year)+"/"+month+"/1");
+}
+
+//傳回下一季一號的date物件
+nextSeasonFirstDay=function(strDate){
+	date=new Date(strDate);
+	year=date.getYear();
+	month=date.getMonth();
+	if(month<=8){
+		month=3*(Math.floor(month/3))+4;
+	}else{
+		month=1;
+		year+=1;
+	}
+	return new Date(ad(year)+"/"+month+"/1");
+
 }
